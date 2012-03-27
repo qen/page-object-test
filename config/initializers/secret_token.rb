67ebc26679993431b7006a7cdbb3f885143185e3
qen <qempaces@cafepress.com>
2012-03-27 17:45:26 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PageObjectApi::Application.config.secret_token = 'e1b8e78db93b49531a79a7116e9dbc2ef6438d94aeffd4d4713fccfa4b73e1eed18b396412bdb2e0dc16d77511674bb37dd619166ed0348293476fc32b5f9d4d'
